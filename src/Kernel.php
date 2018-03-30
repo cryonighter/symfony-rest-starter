d31e056950ac984dd95c5e9992518564dffeb2c9
Cryonighter <cryonighter@andex.ru>
2018-03-30 20:53:20 +0300

    Коммит добра

---
+++ src/Kernel.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App;
+
+use Generator;
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\Config\Resource\FileResource;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+use Symfony\Component\Routing\RouteCollectionBuilder;
+
+class Kernel extends BaseKernel
+{
+    use MicroKernelTrait;
+
+    const CONFIG_EXTS = '.{php,xml,yaml,yml}';
+
+    /**
+     * @return string
+     */
+    public function getCacheDir()
+    {
+        return $this->getProjectDir() . '/var/cache/' . $this->environment;
+    }
+
+    /**
+     * @return string
+     */
+    public function getConfigDir(): string
+    {
+        return $this->getProjectDir() . '/config';
+    }
+
+    /**
+     * @return string
+     */
+    public function getLogDir(): string
+    {
+        return $this->getProjectDir() . '/var/log';
+    }
+
+    /**
+     * @return Generator
+     */
+    public function registerBundles(): Generator
+    {
+        $contents = require $this->getConfigDir() . '/bundles.php';
+
+        foreach ($contents as $class => $envs) {
+            if (isset($envs['all']) || isset($envs[$this->environment])) {
+                yield new $class();
+            }
+        }
+    }
+
+    /**
+     * @param ContainerBuilder $container
+     * @param LoaderInterface  $loader
+     */
+    protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader): void
+    {
+        $confDir = $this->getConfigDir();
+
+        $container->addResource(new FileResource("$confDir/bundles.php"));
+
+        // Feel free to remove the "container.autowiring.strict_mode" parameter
+        // if you are using symfony/dependency-injection 4.0+ as it's the default behavior
+        $container->setParameter('container.autowiring.strict_mode', true);
+        $container->setParameter('container.dumper.inline_class_loader', true);
+
+        $loader->load($confDir . '/{packages}/*' . self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir . '/{packages}/' . $this->environment . '/**/*' . self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir . '/{services}' . self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir . '/{services}_' . $this->environment . self::CONFIG_EXTS, 'glob');
+    }
+
+    /**
+     * @param RouteCollectionBuilder $routes
+     */
+    protected function configureRoutes(RouteCollectionBuilder $routes): void
+    {
+        $confDir = $this->getConfigDir();
+
+        $routes->import($confDir . '/{routes}/*' . self::CONFIG_EXTS, '/', 'glob');
+        $routes->import($confDir . '/{routes}/' . $this->environment . '/**/*' . self::CONFIG_EXTS, '/', 'glob');
+        $routes->import($confDir . '/{routes}' . self::CONFIG_EXTS, '/', 'glob');
+    }
+}
